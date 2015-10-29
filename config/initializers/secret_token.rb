b445f2236a200dbd85951b31631d8caafd14c009
Ingrid <olmccb@hotmail.com>
2015-10-29 13:51:36 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sagecracks::Application.config.secret_key_base = 'f570d9a7f08c09a51677da3333ef974d3d0ee6508252c78e9c39c10fcc454e34a3b0818e95236eae11c7c86e45387836de840f0e0d8efab85fc54d8d9f190209'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sagecracks::Application.config.session_store :cookie_store, key: '_sagecracks_session'
